a2db6eb41d1b67a0d74ffaf25a323acb95685daf
GurtejPunnia <48194711+GurtejPunnia@users.noreply.github.com>
2019-09-05 14:58:47 -0600

    close #3

--- src-1/repo-1/Program.cs
+++ src-1/repo-1/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http; 
+using static System.Console;
+
 
 namespace repo_1
 {
+   
     class Program
     {
+        const string API_BASE = "https://www.gitignore.io/api/";
+        const string API_LIST_LINES = API_BASE + "list? format=lines";
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+            var response = client.GetAsync
+                (API_LIST_LINES).GetAwaiter().GetResult();
+            if(response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync
+                    ().GetAwaiter().GetResult();
+                WriteLine(content);
+                
+
+            }
+
+
+           
         }
     }
 }
